b9e8edbd79709dba15248fbabb112912efcd87b6
Dumplings <dumplings0415@gmail.com>
2018-08-30 01:50:04 +0800

    initialize

---
+++ test/index.js
@@ -0,0 +1,12 @@
+const fixtures = require('./fixtures')
+const getArray = require('../')
+const tape = require('tape')
+const stringify = JSON.stringify
+fixtures.forEach(function (f) {
+  tape('returns ' + stringify(f.expected) + ' for ' + stringify(f.array), function (t) {
+    t.plan(1)
+
+    const actual = getArray(f.array)
+    t.equal(actual.toString(), f.expected.toString())
+  })
+})
